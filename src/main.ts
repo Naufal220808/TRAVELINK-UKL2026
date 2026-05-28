829a43826fb52898ddb96a28b43aa0fc379913c7
nayakanaufal <nayaka_aziz_33rpl@student.smktelkom-mlg.sch.id>
2026-05-28 07:36:01 +0700

    enable cors and fix build

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
   }));
 
   app.setGlobalPrefix('api');
+  app.enableCors();
 
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Server running on http://localhost:${process.env.PORT ?? 3000}/api`);
